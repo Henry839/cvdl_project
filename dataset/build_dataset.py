eca59d838498dacced5290db70c4cff8ac87f1be
Henry839 <2000017754@stu.pku.edu.cn>
2023-06-25 09:25:10 +0800

    dataset prepared

---
+++ dataset/build_dataset.py
@@ -0,0 +1,26 @@
+# Announcement:
+# 1. 9 types
+# 2. {type}:{frame_num}-- {dressage}:3589 {blueboy}:1416 {rat}:2606 {car}:1109 {dog}:891 {parkour}:1578 {skating}:778 {skiing}:692 {skiing-long}:903
+# 3. 8 frames / data
+import os
+from os import listdir
+from os.path import isfile, join
+
+root_path = "./JPEGImages"
+type_name = ['blueboy','car','dog','dressage','parkour_boy','rat','skating','skiing','skiing_slalom']
+print("Begin")
+
+for t in type_name:
+	path = join(root_path,t)
+	frames = [f for f in listdir(path) if isfile(join(path, f))]
+	frames.sort()
+	print(len(frames))
+	i = 0 
+	while( (8*i + 7) < len(frames)):
+		os.makedirs(f"./data/{t}/{i}/")
+		for j in range(8*i, 8*i + 8):
+			os.rename(join(path,frames[j]),join(f"./data/{t}/{i}",frames[j]))
+		i = i + 1
+		
+
+
